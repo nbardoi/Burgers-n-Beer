3b647a4f66533afe40b0df5731622e7e570cc1ef
Henry Elliott <hjelliott1987@gmail.com>
2020-01-21 21:24:32 -0600

    MVC setup

---
+++ routes/api-routes.js
@@ -0,0 +1,39 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+// Requiring our models
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // GET route for getting all of the posts
+  app.get("/api/posts", function(req, res) {
+    
+  });
+
+  // Get route for retrieving a single post
+  app.get("/api/posts/:id", function(req, res) {
+    
+  });
+
+  // POST route for saving a new post
+  app.post("/api/posts", function(req, res) {
+    
+  });
+
+  // DELETE route for deleting posts
+  app.delete("/api/posts/:id", function(req, res) {
+    
+  });
+
+  // PUT route for updating posts
+  app.put("/api/posts", function(req, res) {
+    
+  });
+};
