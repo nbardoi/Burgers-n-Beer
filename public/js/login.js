32b52553e76ef5f1b0bbd05bdb3c5d70137e1fb5
Nagma Bardoi <nagmabardoi@gmail.com>
2020-01-28 15:53:55 -0600

    modal login validation

---
+++ public/js/login.js
@@ -0,0 +1,22 @@
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "utsabootcamp" && password == "utsabootcamp19_20"){
+alert ("Login successfully");
+window.location = "/add"; // Redirecting to other page.
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("login").disabled = true;
+return false;
+}
+}
+}
