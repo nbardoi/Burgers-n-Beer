37a782fda4230c1775eac7c4d773fab6f55a9694
Nagma Bardoi <nagmabardoi@gmail.com>
2020-01-28 20:37:29 -0600

    login

--- public/js/login.js
+++ public/js/login.js
@@ -1,22 +1,22 @@
-// var attempt = 3; // Variable to count number of attempts.
+var attempt = 3; // Variable to count number of attempts.
 // Below function Executes on click of login button.
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "utsabootcamp" && password == "test_123"){
-alert ("Login successful");
-window.location = "/add"; // Redirecting to other page.
+alert ("Login successful!");
+window.location = "add"; // Redirecting to other page.
 return false;
 }
-// else{
-// attempt --;// Decrementing by one.
-// alert("You have left "+attempt+" attempt;");
-// // Disabling fields after 3 attempts.
-// // if( attempt == 0){
-// //     document.getElementById("username").disabled = true;
-// //     document.getElementById("password").disabled = true;
-// //     document.getElementById("login").disabled = true;
-// //     return false;
-// // }
-// }
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("login").disabled = true;
+return false;
+}
+}
 }
