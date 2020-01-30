02d49fcc28370ba5eae4c72bfe6645de2cb8eca6
Nagma Bardoi <nagmabardoi@gmail.com>
2020-01-29 21:10:46 -0600

    login button

--- public/js/login.js
+++ public/js/login.js
@@ -10,7 +10,7 @@ return true;
 }
 else{
 attempt --;// Decrementing by one.
-alert("You have left "+attempt+" attempt;");
+alert("You have "+attempt+" attempt(s) left");
 // Disabling fields after 3 attempts.
 if( attempt == 0){
 document.getElementById("username").disabled = true;
